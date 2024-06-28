d618c237b33a6fe9c4948b995a2545a36740b302
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-06-28 16:22:59 -0300

    Update
    Criação de uma lógica para adicionar o catálogo de acordo com a linguagem que o navegador tiver, só esperando uma resposta do cliente pra ver se vai adicionar o catálogo deles ou não.

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react'
 import styles from './Links.module.css'
 import Link from 'next/link'
+
 
 export default function Links() {
 
@@ -7,6 +9,22 @@ export default function Links() {
     const mensagemFormatada = mensagem.replace(' ', '%20')
     const telefone = '5528999044546'
     const site = 'https://www.pedradofrade.com.br/'
+
+    const [language, setLanguage] = useState('')
+
+    useEffect(() => {
+        const browserLanguage = navigator.language || navigator.languages[0]
+        setLanguage(browserLanguage)
+    }, [])
+
+    /* 
+        Aqui eu coloquei um algoritmo que recupera a lingua do navegador
+        Agora é só colocar uma lógica para aparecer o catálogo em inglês 
+        ou português dependendo do lugar onde a pessoa está.
+        só acessar o estado `${language}`, se for pt-BR é só mostrar em 
+        português, senão mostrar em inglês.
+    */
+
 
     return (
         <div className={styles.links_content}>
@@ -20,8 +38,10 @@ export default function Links() {
                 href={site}
                 className={styles.link}
             >
-                Nosso Site
+                Visit our website
             </Link>
+
+
         </div>
     )
 }
